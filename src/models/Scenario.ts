12a807d2e52516a99e0dc836008553684b2dd30d
Marek Kačmár <kacmar.marek@gmail.com>
2024-01-28 19:56:59 +0100

    Scenario and StoredResolvedScenario models added

---
+++ src/models/Scenario.ts
@@ -0,0 +1,36 @@
+interface Scenario {
+  id: string;
+  rules: string;
+  name: string;
+  devices?: Array<string>;
+  deactivated: boolean;
+  isAlreadyTriggered: boolean;
+  mutedUntil: number;
+  createdAt: number;
+  forTimeCountingActivatedAt: Map<string, number>;
+  messageAndTriggerTime: Map<string, Array<number>>;
+  messageMultiplicityCounter: Map<string, number>;
+  deviceAndTag: Map<string, Array<string>>;
+  jobAndTriggerTime: Map<string, Array<number>>;
+  activeAtDay: Array<number>;
+  activeAtHour: Array<number>;
+}
+
+interface ScenarioFrame {
+  rules?: string;
+  name: string;
+  devices?: Array<string>;
+  deactivated: boolean;
+  isAlreadyTriggered?: boolean;
+  mutedUntil?: number;
+  forTimeCountingActivatedAt?: Map<string, number>;
+  messageAndTriggerTime?: Map<string, Array<number>>;
+  messageMultiplicityCounter?: Map<string, number>;
+  deviceAndTag?: Map<string, Array<string>>;
+  jobAndTriggerTime?: Map<string, Array<number>>;
+  activeAtDay?: Array<number>;
+  activeAtHour?: Array<number>;
+  // TODO odstranit moznosti posielania null hodnoty
+}
+
+export type { Scenario, ScenarioFrame };
