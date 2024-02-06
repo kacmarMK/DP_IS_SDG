8da54b6a38b95d706c52ccb838d0922186fb75a0
Marek Kačmár <kacmar.marek@gmail.com>
2024-02-06 18:27:18 +0100

    All/active scenarios view added, scenario pages added

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -81,8 +81,12 @@ const routes: RouteRecordRaw[] = [
         component: () => import('pages/modules/ModuleDetailPage.vue'),
       },
       {
-        path: 'scenarios',
+        path: '/scenarios',
         component: () => import('pages/scenarios/ScenariosPage.vue'),
+      },
+      {
+        path: '/scenarios/create',
+        component: () => import('pages/scenarios/CreateScenarioPage.vue'),
       },
     ],
   },
