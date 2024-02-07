c84a69032c6ce3f3840a7331e6b46d708f549f54
Marek Šmogrovič <xsmogrovic@stuba.sk>
2024-02-08 00:15:28 +0100

    Subrecipes

--- src/components/recipes/RecipeForm.vue
+++ src/components/recipes/RecipeForm.vue
@@ -19,103 +19,43 @@
           :label="t('device.device_type')"
           :options="Object.values(DeviceTypeEnum)"
         />
-        <q-checkbox v-model="recipe.subRecipe" :disable="props.loading" dense label="Subrecipe" class="col-auto" />
+        <q-checkbox
+          v-model="recipe.subRecipe"
+          :disable="props.loading"
+          dense
+          :label="t('recipe.subrecipe')"
+          class="col-auto"
+        />
       </div>
     </q-card>
-    <div class="q-mt-auto row q-col-gutter-xl">
-      <div class="col-12 col-md-6">
-        <div class="table-name text-secondary">Recipe Steps</div>
-        <VueDraggable v-model="localRecipeCommands" :animation="200" handle=".handle" draggable="tr" target="tbody">
-          <q-table
-            :rows="localRecipeCommands"
-            :columns="stepColumns"
-            flat
-            :no-data-label="`No commands added to this recipe`"
-            :loading-label="t('table.loading_label')"
-            :rows-per-page-label="t('table.rows_per_page_label')"
-            :rows-per-page-options="[0, 20, 50, 100]"
-            hide-pagination
-            class="shadow"
-          >
-            <template #header-cell-drag="propsDrag">
-              <q-th auto-width :props="propsDrag" class="drag-th" />
-            </template>
-
-            <template #body-cell-drag="propsDrag">
-              <q-td auto-width :props="propsDrag">
-                <div>
-                  <q-icon :name="mdiDrag" size="28px" class="handle drag-icon" />
-                </div>
-              </q-td>
-            </template>
-
-            <template #body-cell-step="propsStep">
-              <q-td auto-width :props="propsStep">
-                <div>{{ propsStep.rowIndex + 1 }}</div>
-              </q-td>
-            </template>
-
-            <template #body-cell-remove="propsRemove">
-              <q-td auto-width :props="propsRemove">
-                <div>
-                  <q-btn
-                    dense
-                    :icon="mdiClose"
-                    color="red"
-                    flat
-                    round
-                    :disable="props.loading"
-                    @click="removeCommandFromRecipe(propsRemove.rowIndex)"
-                  />
-                </div>
-              </q-td>
-            </template>
-          </q-table>
-        </VueDraggable>
-      </div>
-      <div class="col-12 col-md-6">
-        <div class="table-name text-secondary">Available Commands</div>
-        <q-table
-          :rows="filteredCommands"
-          :columns="availableCommandsColumns"
-          :loading="commandStore.commands.isLoading"
-          flat
-          :no-data-label="`No commands available for this device type`"
-          :loading-label="t('table.loading_label')"
-          :rows-per-page-label="t('table.rows_per_page_label')"
-          :rows-per-page-options="[20, 50, 100, 0]"
-          class="shadow"
-        >
-          <template #body-cell-actions="propsActions">
-            <q-td auto-width :props="propsActions">
-              <q-btn
-                :disable="props.loading"
-                dense
-                :icon="mdiPlus"
-                color="primary"
-                flat
-                round
-                @click="addCommandToRecipe(propsActions.row)"
-              />
-            </q-td>
-          </template>
-        </q-table>
-      </div>
-    </div>
+    <q-btn-toggle
+      v-model="selectedStepType"
+      spread
+      class="q-my-lg shadow custom-toggle"
+      no-caps
+      rounded
+      unelevated
+      toggle-color="primary"
+      color="white"
+      text-color="primary"
+      :options="[
+        { label: t('command.label', 2), value: 'commands' },
+        { label: t('recipe.subrecipe', 2), value: 'subrecipes' },
+      ]"
+    />
+    <CommandStepsSelect v-show="selectedStepType === 'commands'" v-model="recipe" :loading="props.loading" />
+    <SubrecipeStepsSelect v-show="selectedStepType === 'subrecipes'" v-model="recipe" :loading="props.loading" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { RecipeInput, getEmptyRecipeInput } from '@/models/Recipe';
-import { QTableProps } from 'quasar';
-import { computed, ref, watch } from 'vue';
+import { ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import DeviceTypeEnum from '@/models/DeviceType';
-import { Command } from '@/models/Command';
-import { mdiPlus, mdiClose, mdiDrag } from '@quasar/extras/mdi-v6';
-import { VueDraggable } from 'vue-draggable-plus';
-import { useCommandStore } from '@/stores/command-store';
 import { isFormValid } from '@/utils/form-validation';
+import CommandStepsSelect from '@/components/recipes/CommandStepsSelect.vue';
+import SubrecipeStepsSelect from './SubrecipeStepsSelect.vue';
 
 const recipe = defineModel<RecipeInput>({ required: true, default: getEmptyRecipeInput() });
 const props = defineProps({
@@ -127,38 +67,8 @@ const props = defineProps({
 defineExpose({ validate });
 
 const { t } = useI18n();
-const commandStore = useCommandStore();
-commandStore.commands.refresh();
 
-const localRecipeCommands = ref(recipe.value.commands?.map((c, index) => ({ id: index, value: c })) ?? []);
-
-const filteredCommands = computed<Command[]>(() => {
-  return (
-    commandStore.commands.data?.filter((command) => {
-      if (!recipe.value.deviceType) {
-        return true;
-      }
-      return command.deviceType === recipe.value.deviceType;
-    }) ?? []
-  );
-});
-
-function addCommandToRecipe(command: Command) {
-  const uniqueId = Date.now() + Math.random();
-  localRecipeCommands.value.push({ id: uniqueId, value: command });
-}
-
-function removeCommandFromRecipe(index: number) {
-  localRecipeCommands.value.splice(index, 1);
-}
-
-watch(
-  localRecipeCommands,
-  (newLocalCommands) => {
-    recipe.value.commands = newLocalCommands.map((item) => item.value);
-  },
-  { deep: true },
-);
+const selectedStepType = ref('commands');
 
 const nameRules = [(val: string) => (val && val.length > 0) || t('global.rules.required')];
 const deviceTypeRules = [(val: string) => (val && val.length > 0) || t('global.rules.required')];
@@ -170,73 +80,10 @@ function validate() {
   const refs = [nameRef.value, deviceTypeRef.value];
   return isFormValid(refs);
 }
-
-const stepColumns = computed<QTableProps['columns']>(() => [
-  {
-    name: 'drag',
-    label: '',
-    field: '',
-    sortable: false,
-    align: 'center',
-  },
-  {
-    name: 'command',
-    label: t('command.label'),
-    field: (row) => row.value.name,
-    sortable: false,
-    align: 'left',
-  },
-  {
-    name: 'remove',
-    label: '',
-    field: '',
-    sortable: false,
-    align: 'center',
-  },
-]);
-
-const availableCommandsColumns = computed<QTableProps['columns']>(() => [
-  {
-    name: 'actions',
-    label: '',
-    field: '',
-    align: 'center',
-    sortable: false,
-  },
-  {
-    name: 'name',
-    label: t('global.name'),
-    field: 'name',
-    sortable: true,
-    align: 'left',
-  },
-  {
-    name: 'parameters',
-    label: t('command.parameters'),
-    field: 'params',
-    sortable: false,
-    align: 'left',
-    format: (val: string[]) => val?.join(', '),
-  },
-]);
 </script>
 
 <style lang="scss" scoped>
-.table-name {
-  font-size: 1.1rem;
-  font-weight: 500;
-  margin-bottom: 10px;
-}
-
-.drag-icon {
-  cursor: move;
-}
-
-.shrink-width {
-  width: 0rem;
-}
-
-.drag-th {
-  width: 60px;
+.custom-toggle {
+  border: 1px solid var(--q-primary);
 }
 </style>
