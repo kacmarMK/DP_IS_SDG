098eb1409a70cef4c64fce4f2dd3c15aca6c0b09
Marek Šmogrovič <xsmogrovic@stuba.sk>
2024-03-09 16:56:36 +0100

    Quasar lang

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,6 +1,9 @@
 import { boot } from 'quasar/wrappers';
 import { createI18n } from 'vue-i18n';
 import messages from '@intlify/unplugin-vue-i18n/messages';
+import { Quasar, QuasarLanguage } from 'quasar';
+
+const langList = import.meta.glob('../../node_modules/quasar/lang/(en-US|sk).mjs');
 
 export default boot(({ app }) => {
   const i18n = createI18n({
@@ -8,6 +11,17 @@ export default boot(({ app }) => {
     locale: localStorage.getItem('locale') ?? 'en',
     messages,
   });
+
+  let langIso = i18n.global.locale.value;
+  if (langIso === 'en') langIso = 'en-US';
+  try {
+    langList[`../../node_modules/quasar/lang/${langIso}.mjs`]().then((lang: unknown) => {
+      const typedLang = lang as { default: QuasarLanguage };
+      Quasar.lang.set(typedLang.default);
+    });
+  } catch (err) {
+    console.error(`Language file for "${langIso}" not found`);
+  }
 
   // Set i18n instance on app
   app.use(i18n);
